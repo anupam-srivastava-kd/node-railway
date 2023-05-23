be917721ef5b68f501cbab1236ec109103c6c528
Anupam Srivastava <108662490+anupam-srivastava-kd@users.noreply.github.com>
2023-05-23 22:44:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const port = 3000;
 
+app.use(express.json());
 
-const port = process.env.PORT || 5002
+app.post('/api/request', (req, res) => {
+  const requestData = req.body;
+  console.log('Received request:', requestData);
 
+  // Process the request data or perform any necessary operations
 
-// addData()
-// app.use(cors())
+  // Send the request details back as the response
+  res.json({
+    success: true,
+    message: 'Request received successfully',
+    requestData: requestData
+  });
+});
 
-//middleware
-app.use(express.json())
-// app.disable('view cache');
-
-//router 
-app.get("/", (req,res)=>{
-    res.send( 'hw' );
-})
-
-
-app.listen(port, ()=>{
-    console.log('server started');
-})
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
